c36577dd9ad75688c910bb39ddd921a6caa28466
tamasubi <hidtamas@gmail.com>
2022-10-07 17:31:13 +0200

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,7 +52,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
+    @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
